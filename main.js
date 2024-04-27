625f3d8c6571c3bd793097579a01768f289a1e29
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2024-04-27 14:24:15 +0200

    Make Task4

--- main.js
+++ main.js
@@ -74,3 +74,57 @@ function customShuffle(array) {
 
 let arrayToBeShuffled = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(customShuffle(arrayToBeShuffled));
+
+// Task 4: Array Intersection and Union
+function getArrayIntersection(array1, array2) {
+    let result = [];
+
+    for (let i = 0; i < array1.length; ++i) {
+        if (array2.indexOf(array1[i]) !== -1)
+            result.push(array1[i]);
+    }
+
+    return result;
+}
+
+function getArrayUnion(array1, array2) {
+    let result = [];
+
+    for (let i = 0; i < array1.length; ++i) {
+        if (array1.lastIndexOf(array1[i]) !== array1.indexOf(array1[i]))
+            continue;
+
+        if (array2.indexOf(array1[i]) === -1) {
+            result.push(array1[i]);
+            continue;
+        }
+
+        if (array2.indexOf[array1[i]] !== array2.lastIndexOf(array1[i]))
+            continue;
+
+        if (result.indexOf(array1[i]) !== -1)
+            continue;
+
+        result.push(array1[i]);
+    }
+
+    for (let i = 0; i < array2.length; ++i) {
+        if (array2.lastIndexOf(array2[i]) !== array2.indexOf(array2[i]))
+            continue;
+
+        if (array1.indexOf(array2[i]) === -1) {
+            result.push(array2[i]);
+            continue;
+        }
+
+        if (array1.indexOf[array2[i]] !== array1.lastIndexOf(array2[i]))
+            continue;
+
+        if (result.indexOf(array2[i]) !== -1)
+            continue;
+
+        result.push(array2[i]);
+    }
+
+    return result;
+}
